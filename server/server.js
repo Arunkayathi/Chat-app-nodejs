71415be31f3b6e8158264e2d4ba91656d29ca23f
arun reddy <arun reddy>
2017-06-10 11:55:48 -0500

    Add index.js and add newMessage ,createMessage Events

--- server/server.js
+++ server/server.js
@@ -12,9 +12,18 @@ var io=socketIo(server);
 
 io.on('connection',(socket)=>{
     console.log('new user is connected');
+    socket.emit('newMessage',{
+        from:'arun',
+        text:"hello everyone",
+        createdAt:123
+    });
+    socket.on('createMessage',(newMessage)=>{
+        console.log('new Message',newMessage);
+    });
     socket.on('disconnect',()=>{
         console.log('user was disconnected');
     });
+
 
 });
 server.listen(port,()=>{
