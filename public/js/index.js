c2297822b821724f032717626117c0710b659e81
arun reddy <arun reddy>
2017-06-12 11:11:46 -0500

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -23,25 +23,37 @@ socket.on('newLocationMessage',function(message){
 
 });
 
-jQuery('#message-form').on('submit',function(e){
-    e.preventDefault();
-    socket.emit('createMessage',{
-        from:'user',
-        text:jQuery('[name=message]').val()
-    });
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text:jQuery('[name=message]').val()
+
+  }, function () {
+
+  jQuery('[name=message]').val(' ');
+
+  });
 });
+
 
 var locationPosition=jQuery('#location');
 locationPosition.on('click',function(){
     if(!navigator.geolocation){
         return alert('Geo location is not supported by the browser');
     }
+    locationPosition.attr('disabled','disabled').text('Sending location...')
     navigator.geolocation.getCurrentPosition(function(position){
+      locationPosition.removeAttr('disabled').text('Send location');
             socket.emit('createLocationMessage',{
                 latitude:position.coords.latitude,
                 longitude:position.coords.longitude
             });
     },function(){
+          locationPosition.removeAttr('disabled').text('Send location');
         alert('unable to fetch location');
     });
 });
