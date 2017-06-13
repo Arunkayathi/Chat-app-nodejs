0a009ac2c380998a86f369ec5e4113cfa519db7b
arun reddy <arun reddy>
2017-06-13 11:32:59 -0500

    Add auto scrolling feature

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,21 @@
 var socket=io();
+
+function scrollToBottom (){
+
+var messages=jQuery('#messages');
+var newMessage=messages.children('li:last-child');
+var clientHeight=messages.prop('clientHeight');
+var scrollHeight=messages.prop('scrollHeight');
+var scrollTop=messages.prop('scrollTop');
+var newMessageHeight=newMessage.innerHeight();
+var lastMessageHeight=newMessage.prev().innerHeight();
+
+
+  if(clientHeight+scrollTop+newMessageHeight+lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+
+}
 socket.on('connect',function(){
         console.log('connected to server');
 
@@ -17,6 +34,7 @@ socket.on('newMessage',function(message){
     createdAt:formattedTime
   });
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 socket.on('newLocationMessage',function(message){
   var formattedTime=moment(message.createdAt).format('h:mm a');
@@ -27,14 +45,12 @@ socket.on('newLocationMessage',function(message){
       url:message.url
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 
 });
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
-
-
-
   socket.emit('createMessage', {
     from: 'User',
     text:jQuery('[name=message]').val()
